a3bec585348652ac995322601803fba1ed68ae26
DavidSark <david.sarkissian@edu.univ-fcomte.fr>
2023-02-26 20:21:24 +0100

    correctif works

--- portfoliov3/src/views/WorksView.vue
+++ portfoliov3/src/views/WorksView.vue
@@ -16,7 +16,7 @@
                 <div class="ligne1 border-default border-blanc_impur "></div>
 
                 <div class="flex justify-center items-center mt-16">
-                     <RouterLink to="/works" class="font-poppins text-base md:text-26 uppercase text-blanc_impur light_anim2 ">scroll down</RouterLink>
+                     <p class="font-poppins text-base md:text-26 uppercase text-blanc_impur light_anim2 ">scroll down</p>
                 </div>
 
             </main>
@@ -24,10 +24,11 @@
         </div>
 
 
-        <ul class=" ">
-            <li class="text-center ">
-                <RouterLink to="/naknow" class="relative font-poppins text-lg md:text-5xl lg:text-5xl 2xl:text-3rem uppercase text-vert_clair random-color tracking-widest letter_spacing" >
-                  <span  @mouseover="showImage('naknowledge')" @mouseleave="hideImage('naknowledge')" class=" name bord_transparent block lg:inline-block font-extrabold hover_effect">Naknowledge</span> <span class="hidden lg:inline-block">-</span> School project
+        <ul class=" flex flex-col justify-center items-center">
+            <li class="">
+              
+                <RouterLink to="/naknow" class="lg:flex lg:justify-between relative font-poppins text-lg md:text-5xl lg:text-5xl 2xl:text-3rem uppercase text-vert_clair random-color tracking-widest letter_spacing" >
+                  <span  @mouseover="showImage('naknowledge')" @mouseleave="hideImage('naknowledge')" class=" name bord_transparent block lg:inline-block font-extrabold hover_effect max-w-6xl lg:w-1/2">Naknowledge</span> <span class="hidden lg:inline-block">|</span> School project
                     
                   <transition name="fade">
                     <img
@@ -42,18 +43,20 @@
                 <hr class="my-10 lg:my-32 test"/>
             </li>
 
-            <li class="text-center ">
-                <RouterLink to="/works" class="relative font-poppins text-lg md:text-5xl lg:text-5xl 2xl:text-3rem uppercase text-vert_clair random-color tracking-widest letter_spacing" >
-                  <span  @mouseover="showImage('country')" @mouseleave="hideImage('country')" class=" name bord_transparent block lg:inline-block font-extrabold 
-                  hover_effect">Ghostie</span> <span class="hidden lg:inline-block">-</span> School project
+            
+
+            <li class="">
+                <RouterLink to="/works" class="lg:flex lg:justify-between relative font-poppins text-lg md:text-5xl lg:text-5xl 2xl:text-3rem uppercase text-vert_clair random-color tracking-widest letter_spacing" >
+                  <span  @mouseover="showImage('ghostie')" @mouseleave="hideImage('ghostie')" class=" name bord_transparent block lg:inline-block font-extrabold 
+                  hover_effect max-w-6xl lg:w-1/2">Ghostie</span> <span class="hidden lg:inline-block ">|</span> School project
 
                   <transition name="fade">
                     <img 
-                    v-show="showCountryImage" 
+                    v-show="showGhostieImage" 
                       class="hidden w-30rem lg:inline-block absolute bottom-52 right-0 ml-2 2xl:bottom-64 2xl:w-32.5rem h-auto transform translate-y-full"
                       src="../assets/img/Ghostie.PNG"
                       alt="Image"
-                      :style="countryImageStyle" />
+                      :style="ghostieImageStyle" />
                   </transition>
                 </RouterLink>
 
@@ -71,7 +74,7 @@
         <section class="my-40">
     <div class="flex justify-center items-center space-x-20 lg:space-x-44 uppercase">
         <RouterLink to="/works" class="light_anim4">Works</RouterLink>
-        <RouterLink to="/" class="light_anim4 ">Test</RouterLink>
+        <RouterLink to="/about" class="light_anim4 ">About</RouterLink>
     </div>
 </section>
 
@@ -85,9 +88,9 @@ export default {
     data() {
       return {
         showNaknowledgeImage: false,
-        showCountryImage: false,
+        showGhostieImage: false,
         naknowledgeImageStyle: {},
-        countryImageStyle: {}
+        ghostieImageStyle: {}
       }
     },
     methods: {
